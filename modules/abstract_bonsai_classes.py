passed test and fixed memory leakage, needs more performance testsing

modules/abstract_bonsai_classes.py
@@ -1,14 +1,18 @@
 from typing import Dict, Any
 import torch
 from torch import nn
+import weakref
 
 
 class BonsaiModule(nn.Module):
 
     def __init__(self, bonsai_model: nn.Module, module_cfg: Dict[str, Any]):
         super(BonsaiModule, self).__init__()
-        self.bonsai_model = bonsai_model
+        self.bonsai_model = weakref.ref(bonsai_model)
         self.module_cfg = module_cfg
+
+    def get_model(self):
+        return self.bonsai_model()
 
     def forward(self, layer_input):
         raise NotImplementedError
@@ -49,6 +53,12 @@ class Prunable(BonsaiModule):
     def forward(self, layer_input):
         raise NotImplementedError
 
+    def reset(self):
+        self.weights = None
+        self.activation = None
+        self.grad = None
+        self.ranking = torch.zeros(self.module_cfg["out_channels"])
+
     def calc_layer_output_size(self, input_size):
         raise NotImplementedError
 
@@ -63,9 +73,6 @@ class Prunable(BonsaiModule):
     @staticmethod
     def prune_output(pruning_targets, module_name, module_tensor):
         raise NotImplementedError
-
-    def reset(self):
-        self.ranking = torch.zeros(self.module_cfg["out_channels"])
 
     def propagate_pruning_target(self, initial_pruning_targets=None):
         raise NotImplementedError

modules/bonsai_model.py
@@ -1,7 +1,7 @@
 import copy
+import weakref
 from collections import Counter
 from typing import List
-import numpy as np
 import torch
 from torch import nn
 from modules.errors import NotBonsaiModuleError
@@ -12,26 +12,11 @@ from modules.model_cfg_parser import basic_model_cfg_parsing
 
 class BonsaiModel(torch.nn.Module):
 
-    class _Mediator:
-        """
-        Used to mediate between Bonsai model and its modules, while avoiding circular referencing of torch.nn.Modules
-        """
-        def __init__(self, model=None):
-            super().__init__()
-            self.model = model
-
-        def __getattribute__(self, item):
-            try:
-                return super().__getattribute__(item)
-            except AttributeError:
-                return self.model.__getattribute__(item)
-
     def __init__(self, cfg_path, bonsai):
         super(BonsaiModel, self).__init__()
-        self.bonsai = bonsai
+        self.bonsai = weakref.ref(bonsai)
         self.device = None
 
-        self._mediator = self._Mediator(self)
         self.output_channels: List[int] = []
         self.output_sizes: List = []
 
@@ -50,6 +35,9 @@ class BonsaiModel(torch.nn.Module):
     def __call__(self, *args, **kwargs):
         return super().__call__(*args, **kwargs)
 
+    def get_bonsai(self):
+        return self.bonsai()
+
     def _reset_forward(self):
 
         self.model_output = []
@@ -65,7 +53,9 @@ class BonsaiModel(torch.nn.Module):
             if module.module_cfg.get("output"):
                 self.model_output.append(x)
 
-        return self.model_output
+        output = self.model_output
+        self._reset_forward()
+        return output
 
     # TODO needs design for nn.Linear construction, including feature map size, strides, kernel sizes, etc.
     def _create_bonsai_modules(self) -> nn.ModuleList:
@@ -88,13 +78,8 @@ class BonsaiModel(torch.nn.Module):
             # get the module creator based on type
             module_creator = BonsaiFactory.get_creator(module_type)
             # create the module using the creator and module cfg
-            module = module_creator(self._mediator, module_cfg)
+            module = module_creator(self, module_cfg)
             self.output_sizes.append(module.calc_layer_output_size(self.output_sizes[-1]))
-            # parsed_cfg = module.module_cfg
-            # n_out = np.floor(n_in + 2 * padding - kernel_size / stride) -1 #activation map size
-            # jump_out = jump_in * stride #jump in features (equivalent to the accumulated stride)
-            # r_out = r_in + (kernel_size-1) * j_in
-            # start_out = start_in + ( (kernel_size - 1) / 2 - p) * j_in # note: can be discarded
 
             module_list.append(module)
         return module_list

modules/bonsai_modules.py
@@ -113,8 +113,8 @@ class PBConv2d(AbstractBConv2d, Prunable):
     def forward(self, layer_input):
         x = self.conv2d(layer_input)
 
-        if self.bonsai_model.to_rank:
-            self.bonsai_model.bonsai.prunner.attach_hooks_for_rank_calculation(self, x)
+        if self.get_model().to_rank:
+            self.get_model().get_bonsai().prunner.attach_hooks_for_rank_calculation(self, x)
 
         if self.f is not None:
             x = self.f(x)
@@ -208,8 +208,8 @@ class PBDeconv2d(AbstractBDeconv2d, Prunable):
     def forward(self, layer_input):
         x = self.deconv2d(layer_input)
 
-        if self.bonsai_model.to_rank:
-            self.bonsai_model.bonsai.prunner.attach_hooks_for_rank_calculation(self, x)
+        if self.get_model().to_rank:
+            self.get_model().bonsai.prunner.attach_hooks_for_rank_calculation(self, x)
 
         if self.bn is not None:
             x = self.bn(x)
@@ -241,10 +241,10 @@ class BRoute(BonsaiModule):
         bonsai_model.output_channels.append(out_channels)
 
     def forward(self, layer_input):
-        return torch.cat(tuple(self.bonsai_model.layer_outputs[i] for i in self.module_cfg["layers"]), dim=1)
+        return torch.cat(tuple(self.get_model().layer_outputs[i] for i in self.module_cfg["layers"]), dim=1)
 
     def calc_layer_output_size(self, input_size):
-        prev_layers_output_sizes = [self.bonsai_model.output_sizes[i] for i in self.module_cfg["layers"]]
+        prev_layers_output_sizes = [self.get_model().output_sizes[i] for i in self.module_cfg["layers"]]
         out_c = 0
         out_h = None
         out_w = None
@@ -260,18 +260,18 @@ class BRoute(BonsaiModule):
     # TODO - add more documentation
     def propagate_pruning_target(self, initial_pruning_targets=None):
         # get indices of layers to concat
-        layer_indices = [len(self.bonsai_model.pruning_targets) - 1 - i for i in self.module_cfg["layers"]]
+        layer_indices = [len(self.get_model().pruning_targets) - 1 - i for i in self.module_cfg["layers"]]
         # reset desired output and index buffer
         result = []
         buffer = 0
         # iterate over concatenated layers
         for layer_idx in layer_indices:
             # get module whose output is concatenated
-            module = self.bonsai_model.module_list[layer_idx]
+            module = self.get_model().module_list[layer_idx]
             module_out_channels = module.module_cfg["out_channels"]
 
             if layer_idx in self.keys():
-                module_pruning_targets = self.bonsai_model.pruning_targets[layer_idx]
+                module_pruning_targets = self.get_model().pruning_targets[layer_idx]
             else:
                 module_pruning_targets = list(range(module_out_channels))
             module_pruning_targets = [x + buffer for x in module_pruning_targets]
@@ -312,8 +312,8 @@ class BPixelShuffle(BonsaiModule):
 
     def propagate_pruning_target(self, initial_pruning_targets=None):
         pass
-        # prev_targets = self.bonsai_model.pruning_targets[-1]
-        # self.bonsai_model.pruning_targets.append(prev_targets)
+        # prev_targets = self.get_model.pruning_targets[-1]
+        # self.get_model.pruning_targets.append(prev_targets)
 
 
 class BMaxPool2d(BonsaiModule):
@@ -340,7 +340,7 @@ class BMaxPool2d(BonsaiModule):
         pass
 
     def propagate_pruning_target(self, initial_pruning_targets=None):
-        return self.bonsai_model.pruning_targets[-1]
+        return self.get_model().pruning_targets[-1]
 
 
 class BAvgPool2d(BonsaiModule):
@@ -367,7 +367,7 @@ class BAvgPool2d(BonsaiModule):
         pass
 
     def propagate_pruning_target(self, initial_pruning_targets=None):
-        return self.bonsai_model.pruning_targets[-1]
+        return self.get_model().pruning_targets[-1]
 
 
 class BGlobalAvgPool2d(BonsaiModule):
@@ -387,7 +387,7 @@ class BGlobalAvgPool2d(BonsaiModule):
         pass
 
     def propagate_pruning_target(self, initial_pruning_targets=None):
-        return self.bonsai_model.pruning_targets[-1]
+        return self.get_model().pruning_targets[-1]
 
 
 class BFlatten(BonsaiModule):
@@ -472,4 +472,4 @@ class BDropout(BonsaiModule):
         pass
 
     def propagate_pruning_target(self, initial_pruning_targets=None):
-        return self.bonsai_model.pruning_targets[-1]
+        return self.get_model().pruning_targets[-1]